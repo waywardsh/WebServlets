48e34cf54c0e52994dd9fee2dbc0fad54996fc95
Aaron <s4423@inbox.ru>
2017-05-30 14:52:25 +0600

    Test commit

--- src/main/java/servlets/FServlet.java
+++ src/main/java/servlets/FServlet.java
@@ -1,12 +1,12 @@
 package servlets;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/index.jsp")
 public class FServlet extends HttpServlet{
@@ -19,15 +19,15 @@ public class FServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
             throws ServletException, IOException {
-//        httpServletResponse.setContentType("text/html");
-//        PrintWriter out = httpServletResponse.getWriter();
-//        out.println("Hello World");
-        String varTextA = "Hello World";
-        httpServletRequest.setAttribute("textA", varTextA);
-        String varTextB = "Its JSP";
-        httpServletRequest.setAttribute("textB", varTextB);
-
-        RequestDispatcher dispatcher = httpServletRequest.getRequestDispatcher("/index.jsp");
-        dispatcher.forward(httpServletRequest,httpServletResponse);
+        httpServletResponse.setContentType("text/html");
+        PrintWriter out = httpServletResponse.getWriter();
+        out.println("Hello World");
+//        String varTextA = "Hello World";
+//        httpServletRequest.setAttribute("textA", varTextA);
+//        String varTextB = "Its JSP";
+//        httpServletRequest.setAttribute("textB", varTextB);
+//
+//        RequestDispatcher dispatcher = httpServletRequest.getRequestDispatcher("/index.jsp");
+//        dispatcher.forward(httpServletRequest,httpServletResponse);
     }
 }
